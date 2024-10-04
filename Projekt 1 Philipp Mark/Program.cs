f2787eabb9160e767d9c29a9eda843dd52a596f2
Philipp Mark <philipp.mark@student.htldornbirn.at>
2024-10-04 10:35:37 +0200

    Aufgabe 1 verspätete Abgabe

--- Projekt 1 Philipp Mark/Program.cs
+++ Projekt 1 Philipp Mark/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, SWP");
+bool isInput = true;
+
+
+Console.WriteLine("Schreibe deinen Namen!");
+
+while (isInput) {
+    string input = Console.ReadLine();
+    Console.WriteLine(input);
+    
+    if(input =="Beenden") {
+        isInput = false;
+}}
